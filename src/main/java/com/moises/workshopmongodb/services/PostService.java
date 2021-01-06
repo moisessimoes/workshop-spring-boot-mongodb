51cfc8ade41a686227049e351fa21cd3155c7ae7
Moises Simoes <moisessimoes02@gmail.com>
2021-01-06 15:35:33 -0300

    Consulta simples com query methods

--- src/main/java/com/moises/workshopmongodb/services/PostService.java
+++ src/main/java/com/moises/workshopmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.moises.workshopmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,11 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	
 	//====================================================================================================================
 	
-	
+	public List<Post> findByTitle(String title) {
+		
+		return postRepository.findByTitleContainingIgnoreCase(title);
+	}
 }
